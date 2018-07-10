28753b07fbfbafa503fb13fc40860090f3fe7aec
LS-COM-00056 <LS-COM-00056@LS-COM-00056>
2018-07-10 16:35:29 +0900

    css test

---
+++ searchHospital/src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HomeController {
+	@RequestMapping(value="/",method=RequestMethod.GET)
+	public String hello(Model model) {
+		model.addAttribute("title", "Spring Boot View Demo");
+        model.addAttribute("time", new Date().toString());
+		return "home";
+	}
+	
+}
