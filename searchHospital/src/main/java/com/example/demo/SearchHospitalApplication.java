80f6be0bb334f7b95a51ec4c5c6d190ce4cc3d34
LS-COM-00056 <LS-COM-00056@LS-COM-00056>
2018-07-10 11:50:03 +0900

    test version_soeun

---
+++ searchHospital/src/main/java/com/example/demo/SearchHospitalApplication.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchHospitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchHospitalApplication.class, args);
+	}
+}
